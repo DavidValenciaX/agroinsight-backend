d4bf22bf75cd2e81b98aa5ccbe423c7cb7de2cb7
DavidValencia526 <davidvalencia0526@gmail.com>
2024-10-12 03:22:02 -0500

    fix: Users could list farm plots where they were not administrators

--- app/plot/application/list_plots_use_case.py
+++ app/plot/application/list_plots_use_case.py
@@ -1,9 +1,10 @@
 # app/plot/application/list_plots_use_case.py
 from sqlalchemy.orm import Session
+from app.farm.infrastructure.sql_repository import FarmRepository
 from app.plot.infrastructure.sql_repository import PlotRepository
 from app.plot.domain.schemas import PlotResponse, PaginatedPlotListResponse
 from app.user.domain.schemas import UserInDB
-from app.infrastructure.common.common_exceptions import DomainException
+from app.infrastructure.common.common_exceptions import DomainException, InsufficientPermissionsException
 from app.infrastructure.mappers.response_mappers import map_plot_to_response
 from fastapi import status
 from math import ceil
@@ -12,7 +13,7 @@ class ListPlotsUseCase:
     def __init__(self, db: Session):
         self.db = db
         self.plot_repository = PlotRepository(db)
-
+        self.farm_repository = FarmRepository(db)
     def list_plots(self, current_user: UserInDB, finca_id: int, page: int, per_page: int) -> PaginatedPlotListResponse:
         farm = self.plot_repository.get_farm_by_id(finca_id)
         if not farm:
@@ -21,11 +22,8 @@ class ListPlotsUseCase:
                 status_code=status.HTTP_404_NOT_FOUND
             )
             
-        if not self.user_has_access_to_farm(current_user.id, finca_id):
-            raise DomainException(
-                message="No tienes acceso a esta finca.",
-                status_code=status.HTTP_403_FORBIDDEN
-            )
+        if not self.farm_repository.user_is_farm_admin(current_user.id, finca_id):
+            raise InsufficientPermissionsException()
 
         total_plots, plots = self.plot_repository.list_plots_by_farm_paginated(finca_id, page, per_page)
 
@@ -41,6 +39,3 @@ class ListPlotsUseCase:
             per_page=per_page,
             total_pages=total_pages
         )
-
-    def user_has_access_to_farm(self, user_id: int, finca_id: int) -> bool:
-        return self.plot_repository.check_user_farm_access(user_id, finca_id)
