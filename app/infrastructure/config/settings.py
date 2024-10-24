9c2d6eb1308efce40da5f6d986a176cc8a648d16
DavidValencia526 <davidvalencia0526@gmail.com>
2024-10-24 13:33:57 -0500

    fix: parse int env variables numbers

--- app/infrastructure/config/settings.py
+++ app/infrastructure/config/settings.py
@@ -5,5 +5,5 @@ load_dotenv(override=True)
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM', 'HS256')
-WARNING_TIME = os.getenv('WARNING_TIME', 1)
+WARNING_TIME = int(os.getenv('WARNING_TIME', 1))
 DEFAULT_EXPIRATION_TIME = int(os.getenv('DEFAULT_EXPIRATION_TIME', 10))
